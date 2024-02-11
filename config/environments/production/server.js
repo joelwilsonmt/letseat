bb271b105c870bda64d41d65fc2bf6debf0e898a
Joel <43355957+joelwilsonmt@users.noreply.github.com>
2024-02-11 14:08:01 -0700

    console logs

--- config/environments/production/server.js
+++ config/environments/production/server.js
@@ -1,7 +1,9 @@
-module.exports = ({ env }) => ({
-    url: env("MY_HEROKU_URL"),
+module.exports = ({ env }) =>
+{
+    console.log("MY_HEROKU_URL",env("MY_HEROKU_URL"));
+   return { url: env("MY_HEROKU_URL"),
     proxy: true,
     app: {
       keys: env.array("APP_KEYS",['keys','keys'])
-    }
-  });
+    }}
+  };
